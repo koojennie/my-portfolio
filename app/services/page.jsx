79e390d2f795d9ed8e6d0bbfe07ffee6c1f6978c
Alia Jennifer <aliyajenny14@gmail.com>
2024-12-11 18:40:09 +0700

    service page 1:26

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,6 +1,64 @@
+"use client";
+
+import { BsArrowDownRight } from 'react-icons/bs';
+import Link from "next/link";
+
+const services = [
+  {
+    num: '01',
+    title: 'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tenetur.',
+    href: "",
+  },
+  {
+    num: '02',
+    title: 'UI/UX Design',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tenetur.',
+    href: "",
+  },
+  {
+    num: '03',
+    title: 'Logo Design',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tenetur.',
+    href: "",
+  },
+  {
+    num: '04',
+    title: 'SEO',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tenetur.',
+    href: "",
+  },
+];
+
+import { motion } from 'framer-motion';
+
 const Services = () => {
   return (
-    <div>services page</div>
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"},}} className="grid grid-cols-1 md:grid-cols-2 gap-[60px]">
+          {services.map((service, index)=> {
+            return (
+              <div key={index}>
+              {/* top */}
+              <div>
+                <div>{service.num}</div>
+                <Link href={service.href}>
+                  <BsArrowDownRight />
+                </Link>
+              </div>
+              {/* heading */}
+              <h2>{service.title}</h2>
+              {/* description */}
+              <p>{service.description}</p>
+              {/* border */}
+              <div className="border-b border-white/20 w-full"></div>
+            </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
   )
 };
 
